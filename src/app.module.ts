73ad7fcf847903e11e958fa8e1c52db9eda7cf67
Luiz <luiz.ssantos.ext@infracommerce.com.br>
2023-08-25 22:38:14 -0300

    feat: add figther module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,18 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { DatabaseModule } from './database/database.module';
+import { FighterModule } from './fighter/fighter.module';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath: '.env',
+    }),
+    DatabaseModule,
+    FighterModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
