3ec1e4edc3214480a398af742dc616d9c1fb8a36
Luiz <luiz.ssantos.ext@infracommerce.com.br>
2023-08-27 22:09:22 -0300

    feat: add env

--- src/database/database.module.ts
+++ src/database/database.module.ts
@@ -17,11 +17,11 @@ import { Statistics } from 'src/entitys/statistics.entity';
 
       useFactory: async () => ({
         type: 'postgres',
-        host: 'localhost',
-        port: 5432,
-        username: 'postgres',
-        password: 'password',
-        database: 'mma',
+        host: process.env.PG_HOST,
+        port: +process.env.PG_PORT,
+        username: process.env.PG_USERNAME,
+        password: process.env.PG_PASSWORD,
+        database: process.env.PG_DATABASE,
         entities: [Fighter, Statistics, Event, Fight, Ranking],
         synchronize: true,
       }),
